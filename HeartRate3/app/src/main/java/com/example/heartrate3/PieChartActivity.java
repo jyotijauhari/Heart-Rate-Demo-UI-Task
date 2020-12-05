a1c92fe5652f3855d962904897cf08ee42154c93
JyotiJauhari <jyotijauhari222@gmail.com>
2020-12-05 22:58:39 +0530

    Task

---
+++ HeartRate3/app/src/main/java/com/example/heartrate3/PieChartActivity.java
@@ -0,0 +1,14 @@
+package com.example.heartrate3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PieChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_chart);
+    }
+}
